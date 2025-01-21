7cdcf528c9acd0d45c6538585e05f32c050324c8
FinchyMcFinch <77432335+FinchyMcFinch@users.noreply.github.com>
2025-01-21 10:54:55 +1000

    new creative tabs added for great, swell and pedal divisions. diapason sounds updated, adding trompette and piccolo pipes.

--- src/main/java/com/finchy/pipeorgans/init/AllCreativeModeTabs.java
+++ src/main/java/com/finchy/pipeorgans/init/AllCreativeModeTabs.java
@@ -13,7 +13,7 @@ public class AllCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PipeOrgans.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> PIPE_ORGANS_BLOCKS = CREATIVE_MODE_TABS.register("pipe_organs_blocks",
+    public static final RegistryObject<CreativeModeTab> PIPE_ORGANS_GREAT = CREATIVE_MODE_TABS.register("pipe_organs_great",
             () -> CreativeModeTab.builder()
                     .icon(() -> new ItemStack(AllBlocks.DIAPASON.get()))
                     .title(Component.translatable("creativetab.pipe_organs_tab"))
@@ -21,6 +21,33 @@ public class AllCreativeModeTabs {
 
                         output.accept(AllBlocks.GEDECKT.get());
                         output.accept(AllBlocks.DIAPASON.get());
+                        output.accept(AllBlocks.GAMBA.get());
+
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> PIPE_ORGANS_SWELL = CREATIVE_MODE_TABS.register("pipe_organs_swell",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(AllBlocks.DIAPASON.get()))
+                    .title(Component.translatable("creativetab.pipe_organs_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(AllBlocks.GEDECKT.get());
+                        output.accept(AllBlocks.DIAPASON.get());
+                        output.accept(AllBlocks.GAMBA.get());
+
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> PIPE_ORGANS_PEDAL = CREATIVE_MODE_TABS.register("pipe_organs_pedal",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(AllBlocks.DIAPASON.get()))
+                    .title(Component.translatable("creativetab.pipe_organs_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(AllBlocks.GEDECKT.get());
+                        output.accept(AllBlocks.DIAPASON.get());
+                        output.accept(AllBlocks.GAMBA.get());
 
                     })
                     .build());
