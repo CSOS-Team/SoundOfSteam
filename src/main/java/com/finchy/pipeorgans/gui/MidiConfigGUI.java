5f9ff8b9900efdd2b609e37cc5fd7942caa294e6
FinchyMcFinch <77432335+FinchyMcFinch@users.noreply.github.com>
2025-03-24 22:51:53 +1000

    added prev and next buttons to midi config gui

--- src/main/java/com/finchy/pipeorgans/gui/MidiConfigGUI.java
+++ src/main/java/com/finchy/pipeorgans/gui/MidiConfigGUI.java
@@ -15,13 +15,15 @@ import java.util.List;
 public class MidiConfigGUI extends Screen {
 
     private ResourceLocation guiTexture;
-    private final int GUI_WIDTH = 176;
-    private final int GUI_HEIGHT = 166;
-    private final int TEXTURE_DIMENSIONS = 256;
+    private final int GUI_WIDTH = 208;
+    private final int GUI_HEIGHT = 134;
+
+    private int cornerX;
+    private int cornerY;
 
     private MidiInputDeviceManager midiInputDeviceManager;
     private List<MidiDevice> availableMidiDevices;
-    private int visibleDeviceId = 0;
+    private int selectedDeviceIndex = 1; // from 1 to however many devices there are
 
     protected MidiConfigGUI(String translatableTitle) {
         super(Component.translatable(translatableTitle));
@@ -30,14 +32,46 @@ public class MidiConfigGUI extends Screen {
         // get available input devices
         availableMidiDevices = midiInputDeviceManager.getAvailableDevices();
 
-        guiTexture = PipeOrgans.asResource("textures/gui/inventory.png");
-        // add left and right buttons
-        addRenderableWidget(net.minecraft.client.gui.components.Button.builder(Component.literal("<"), b -> setCurrentPage(this.pages.get(Math.max(this.pages.indexOf(this.currentPage) - 1, 0)))).pos(leftPos,  topPos - 50).size(16, 16).build());
+        guiTexture = PipeOrgans.asResource("textures/gui/midi_config.png");
+        reevaluateCorners();
+    }
+
+    private void previousDevice() {
+        selectedDeviceIndex = selectedDeviceIndex > 1 ? selectedDeviceIndex - 1 : availableMidiDevices.size();
+    }
+
+    private void nextDevice() {
+        selectedDeviceIndex = selectedDeviceIndex < availableMidiDevices.size() ? selectedDeviceIndex + 1 : 1;
     }
 
     @Override
     public boolean isPauseScreen() {
         return false;
+    }
+
+    @Override
+    public void resize(Minecraft pMinecraft, int pWidth, int pHeight) {
+        reevaluateCorners();
+        super.resize(pMinecraft, pWidth, pHeight);
+    }
+
+    @Override
+    protected void init() {
+        // add left and right buttons
+        addRenderableWidget(net.minecraft.client.gui.components.Button.builder(
+                Component.literal("<"),
+                b -> previousDevice())
+                .pos(cornerX+7,  cornerY+47).size(16, 16).build());
+        addRenderableWidget(net.minecraft.client.gui.components.Button.builder(
+                        Component.literal(">"),
+                        b -> nextDevice())
+                .pos(cornerX+149,  cornerY+47).size(16, 16).build());
+
+    }
+
+    private void reevaluateCorners() {
+        cornerX = (Minecraft.getInstance().getWindow().getGuiScaledWidth()-GUI_WIDTH)/2;
+        cornerY = (Minecraft.getInstance().getWindow().getGuiScaledHeight()-GUI_HEIGHT)/2;
     }
 
     @Override
@@ -50,8 +84,6 @@ public class MidiConfigGUI extends Screen {
     }
 
     private void renderGraphics(GuiGraphics graphics) {
-        int cornerX = (Minecraft.getInstance().getWindow().getGuiScaledWidth()-GUI_WIDTH)/2;
-        int cornerY = (Minecraft.getInstance().getWindow().getGuiScaledHeight()-GUI_HEIGHT)/2;
         graphics.blit(guiTexture, cornerX, cornerY, 0, 0, GUI_WIDTH, GUI_HEIGHT);
     }
 }
