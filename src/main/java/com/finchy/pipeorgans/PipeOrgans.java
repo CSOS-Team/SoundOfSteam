dcbaf661d33cac9d81ab98cb67ac974197b7eb12
FinchyMcFinch <77432335+FinchyMcFinch@users.noreply.github.com>
2025-07-01 18:08:55 +0800

    progress on fixing crash upon placing pipes

--- src/main/java/com/finchy/pipeorgans/PipeOrgans.java
+++ src/main/java/com/finchy/pipeorgans/PipeOrgans.java
@@ -2,18 +2,16 @@ package com.finchy.pipeorgans;
 
 import com.finchy.pipeorgans.init.*;
 import com.mojang.logging.LogUtils;
+import com.tterrag.registrate.util.RegistrateDistExecutor;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
@@ -36,31 +34,18 @@ public class PipeOrgans
         AllItems.register(modEventBus);
         AllSoundEvents.register(modEventBus);
 
+        RegistrateDistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> PipeOrgansClient.onCtorClient(modEventBus, NeoForge.EVENT_BUS));
 
         NeoForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
 
         container.registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {}
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {}
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {}
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            AllPartialModels.init();
-        }
-    }
 
     public static ResourceLocation asResource(String path) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
