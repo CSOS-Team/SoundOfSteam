1ea4a1e11417bba159f1b088cf37a007b2fed0c0
crackheadakira <lasn.mine@gmail.com>
2025-12-22 04:25:49 +0100

    prepare environment for making ponder scenes

--- src/main/java/com/finchy/pipeorgans/PipeOrgansClient.java
+++ src/main/java/com/finchy/pipeorgans/PipeOrgansClient.java
@@ -3,6 +3,8 @@ package com.finchy.pipeorgans;
 import com.finchy.pipeorgans.init.AllPartialModels;
 import com.finchy.pipeorgans.init.AllParticleTypes;
 import com.finchy.pipeorgans.midi.client.ClientMidiLoader;
+import com.finchy.pipeorgans.ponder.POPonderPlugin;
+import net.createmod.ponder.foundation.PonderIndex;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -17,6 +19,7 @@ public class  PipeOrgansClient {
 
     public static void clientInit(final FMLClientSetupEvent event) {
         AllPartialModels.init();
+        PonderIndex.addPlugin(new POPonderPlugin());
     }
 
 }
