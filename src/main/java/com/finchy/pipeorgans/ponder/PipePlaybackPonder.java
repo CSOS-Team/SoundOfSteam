18ac6fd266730d121b5e5cd167b195f0b55adb8b
crackheadakira <lasn.mine@gmail.com>
2025-12-22 21:56:48 +0100

    added ponder scene for playing pipes with a boiler

--- src/main/java/com/finchy/pipeorgans/ponder/PipePlaybackPonder.java
+++ src/main/java/com/finchy/pipeorgans/ponder/PipePlaybackPonder.java
@@ -93,4 +93,44 @@ public class PipePlaybackPonder {
 
         scene.markAsFinished();
     }
+
+    public static void boilerPipePlaying(SceneBuilder builder, SceneBuildingUtil util) {
+        CreateSceneBuilder scene = new CreateSceneBuilder(builder);
+
+        scene.title("boiler_pipe_playback", "Playing pipes with a boiler");
+        scene.configureBasePlate(0, 0, 5);
+
+        PonderUtil.revealBasePlate(scene, util, PonderTimings.BUILD_STEP);
+
+        BlockPos magma = util.grid().at(2, 1, 2);
+        BlockPos fluidTank = util.grid().at(2, 2, 2);
+        BlockPos diapason = util.grid().at(2, 3, 2);
+        BlockPos fluidTankLever = fluidTank.subtract(new Vec3i(0, 0, 1));
+
+        PonderUtil.revealBlock(scene, util, magma, PonderTimings.BUILD_STEP);
+        PonderUtil.revealBlock(scene, util, fluidTank, PonderTimings.BUILD_STEP);
+        PonderUtil.revealBlock(scene, util, diapason, PonderTimings.BUILD_STEP);
+        PonderUtil.revealBlock(scene, util, fluidTankLever,  Direction.SOUTH, PonderTimings.BUILD_STEP);
+
+        scene.overlay().showText(PonderTimings.READING_TIME)
+                .text("Pipes can be played by placing them on top of a boiler")
+                .placeNearTarget()
+                .attachKeyFrame()
+                .pointAt(diapason.getCenter());
+
+        scene.idle(PonderTimings.READING_WINDOW);
+
+        scene.overlay().showText(PonderTimings.READING_TIME)
+                .text("Then to activate the pipe a redstone signal must be passed through")
+                .placeNearTarget()
+                .attachKeyFrame()
+                .pointAt(fluidTank.getCenter());
+
+        scene.idle(PonderTimings.READING_BUFFER);
+        scene.world().toggleRedstonePower(util.select().fromTo(fluidTankLever, diapason));
+
+        scene.idle(PonderTimings.afterBuffer());
+
+        scene.markAsFinished();
+    }
 }
