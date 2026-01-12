568fd95477ab4cae941673d0182d92c05b937247
FinchyMcFinch <77432335+FinchyMcFinch@users.noreply.github.com>
2026-01-12 23:24:00 +1000

    alternate clipboard placement system

--- src/main/java/com/finchy/pipeorgans/infrastructure/clipboardAssistedPlacement/ClipboardAssistedPlacementHandler.java
+++ src/main/java/com/finchy/pipeorgans/infrastructure/clipboardAssistedPlacement/ClipboardAssistedPlacementHandler.java
@@ -1,13 +1,20 @@
 package com.finchy.pipeorgans.infrastructure.clipboardAssistedPlacement;
 
 import com.finchy.pipeorgans.PipeOrgans;
+import com.finchy.pipeorgans.content.noteLink.NoteLinkBehaviour;
+import com.finchy.pipeorgans.content.noteLink.NoteLinkBlock;
+import com.finchy.pipeorgans.content.noteLink.NoteLinkBlockEntity;
 import com.finchy.pipeorgans.init.AllBlocks;
+import com.finchy.pipeorgans.util.PipePitch;
+import com.simibubi.create.content.equipment.clipboard.ClipboardOverrides;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
@@ -25,17 +32,39 @@ public class ClipboardAssistedPlacementHandler {
         BlockPos pos = event.getPos();
         Entity entity = event.getEntity();
 
-        if (!(entity instanceof Player player)) return;
-
-        ItemStack offhand = player.getOffhandItem();
-        if (!offhand.is(com.simibubi.create.AllBlocks.CLIPBOARD.asItem())) return;
-
         BlockEntity be = level.getBlockEntity(pos);
-        if (be == null) return;
+        if (!(be instanceof NoteLinkBlockEntity noteLinkBE)) return; // ensure a note link was just placed
 
-        ClipboardAssistedPlacementBehaviour target = BlockEntityBehaviour.get(be, ClipboardAssistedPlacementBehaviour.TYPE);
-        if (target == null) return;
+        if (!(entity instanceof Player player)) return; // return if a zombie somehow placed a note link
 
+        ItemStack offhand = player.getOffhandItem(); // get item in offhand
+        if (!offhand.is(com.simibubi.create.AllBlocks.CLIPBOARD.asItem())) return; // return if player isn't holding a clipboard in offhand
+
+        if (offhand.hasTag() &&
+                offhand.getTag().contains("CopiedValues") &&
+                offhand.getTagElement("CopiedValues").contains("MusicalFrequency")) { // if the clipboard has anything specifically about note links in its NBT
+
+            CompoundTag clipboardTag = offhand.getTagElement("CopiedValues").getCompound("MusicalFrequency"); // get the NBT data relating to note links
+
+            noteLinkBE.applyClipboardSettings(clipboardTag); // change the new note link's settings depending on what's written on the clipboard
+
+            PipePitch next = PipePitch.fromNormalizedName(clipboardTag.getString("Pitch")).next(); // get the pitch above what's written on the clipboard
+            if (next == null) next = PipePitch.HIGHEST; // if it's the maximum pitch, just stay at the maximum
+            clipboardTag.putString("Pitch", next.getNormalizedName()); // put the new pitch onto the clipboard
+
+        } else { // if the clipboard hasn't been used for note links previously
+            ClipboardOverrides.switchTo(ClipboardOverrides.ClipboardType.WRITTEN, offhand); // make the clipboard visually look like it's been written in
+
+            CompoundTag copiedTag = new CompoundTag(); // make a new tag to put in "CopiedValues"
+            CompoundTag noteLinkTag = new CompoundTag(); // make a new tag to put in "MusicalFrequency"
+            noteLinkTag.putString("Pitch", PipePitch.LOWEST.getNormalizedName()); // default to F#-1
+            noteLinkTag.put("Key", ItemStack.EMPTY.serializeNBT()); // default to no key item
+
+            copiedTag.put("MusicalFrequency", noteLinkTag);
+            offhand.getOrCreateTag().put("CopiedValues", copiedTag); // apply the tags to the clipboard
+        }
+
+        /*
         ClipboardAssistedPlacementData data = ClipboardAssistedPlacementData.fromClipboardItem(offhand, level);
         BlockPos srcPos = data.getPosition(be);
 
@@ -66,5 +95,7 @@ public class ClipboardAssistedPlacementHandler {
             data.writeToClipboardItem(offhand, level);
             logger.debug("Removed position from clipboard data");
         }
+
+         */
     }
 }
