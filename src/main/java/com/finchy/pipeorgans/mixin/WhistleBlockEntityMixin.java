34ebe90d439a8a7e2c141758d5861119a999196f
FinchyMcFinch <77432335+FinchyMcFinch@users.noreply.github.com>
2026-01-18 21:23:13 +1000

    cut down size of steam whistle mixins

--- src/main/java/com/finchy/pipeorgans/mixin/WhistleBlockEntityMixin.java
+++ src/main/java/com/finchy/pipeorgans/mixin/WhistleBlockEntityMixin.java
@@ -1,88 +1,32 @@
 package com.finchy.pipeorgans.mixin;
 
+import com.finchy.pipeorgans.content.windchest.WindchestBlock;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.simibubi.create.content.decoration.steamWhistle.WhistleBlock;
 import com.simibubi.create.content.decoration.steamWhistle.WhistleBlockEntity;
-import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
-
-import com.finchy.pipeorgans.content.windchest.WindchestBlock;
-import com.finchy.pipeorgans.content.pipes.generic.GenericPipeBlock;
-
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
-
-import net.createmod.catnip.animation.LerpedFloat;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WhistleBlockEntity.class)
-public abstract class WhistleBlockEntityMixin {
+@Mixin(value = WhistleBlockEntity.class, remap = false)
+public class WhistleBlockEntityMixin {
 
-    @Shadow(remap = false) protected abstract boolean isPowered();
-    @Shadow(remap = false) protected abstract WhistleBlock.WhistleSize getOctave();
-    @Shadow(remap = false) protected abstract FluidTankBlockEntity getTank();
-    @Shadow(remap = false) protected LerpedFloat animation;
-
-    @Shadow(remap = false)
-    protected abstract void tickAudio(WhistleBlock.WhistleSize size, boolean powered);
-
-    @Inject(method = "tick",
-            at = @At("HEAD"),
-            cancellable = true,
-            remap = false)
-    private void pipeorgans$allowWindchestPower(CallbackInfo ci) {
+    @ModifyExpressionValue(
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/decoration/steamWhistle/WhistleBlockEntity;isVirtual()Z")
+    )
+    private boolean virtualOrWindchestActive(boolean original) {
         WhistleBlockEntity self = (WhistleBlockEntity)(Object)this;
 
-        if (!self.getLevel().isClientSide())
-            return;
-
-        FluidTankBlockEntity tank = getTank();
-
-        BlockState state = self.getBlockState();
-        BlockPos pos = self.getBlockPos();
-        Direction attachedDir = WhistleBlock.getAttachedDirection(state);
-        BlockPos attachedPos = pos.relative(attachedDir);
+        BlockPos attachedPos = self.getBlockPos().relative(
+                WhistleBlock.getAttachedDirection(self.getBlockState())
+        );
         BlockState attachedState = self.getLevel().getBlockState(attachedPos);
+        boolean isActive = (attachedState.getBlock() instanceof WindchestBlock windchest)
+                && windchest.isMasterActive(self.getLevel(), attachedState.getValue(WhistleBlock.FACING), attachedPos);
 
-        boolean windchestActive = false;
-
-        if (attachedState.getBlock() instanceof WindchestBlock windchest) {
-            windchestActive = windchest.isMasterActive(
-                    self.getLevel(),
-                    attachedState.getValue(GenericPipeBlock.FACING),
-                    attachedPos
-            );
-        }
-
-        boolean powered =
-                isPowered() &&
-                        (
-                                (tank != null
-                                        && tank.boiler != null
-                                        && tank.boiler.isActive()
-                                        && (tank.boiler.passiveHeat || tank.boiler.activeHeat > 0))
-                                        || windchestActive
-                        );
-
-        animation.chase(
-                powered ? 1 : 0,
-                powered ? .5f : .4f,
-                powered ? LerpedFloat.Chaser.EXP : LerpedFloat.Chaser.LINEAR
-        );
-        animation.tickChaser();
-
-        DistExecutor.unsafeRunWhenOn(
-                Dist.CLIENT,
-                () -> () -> tickAudio(getOctave(), powered)
-        );
-
-        ci.cancel(); // prevent Create client logic
+        return original || isActive;
     }
+
 }
--- src/main/java/com/finchy/pipeorgans/mixin/WhistleBlockMixin.java
+++ src/main/java/com/finchy/pipeorgans/mixin/WhistleBlockMixin.java
@@ -1,39 +1,23 @@
 package com.finchy.pipeorgans.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.decoration.steamWhistle.WhistleBlock;
 import com.finchy.pipeorgans.content.windchest.WindchestBlock;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(WhistleBlock.class)
+@Mixin(value = WhistleBlock.class, remap = false)
 public abstract class WhistleBlockMixin {
 
-    @Inject(
+    @WrapOperation(
             method = "canSurvive",
-            at = @At("HEAD"),
-            cancellable = true
+            at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/fluids/tank/FluidTankBlock;isTank(Lnet/minecraft/world/level/block/state/BlockState;)Z")
     )
-    private void pipeorgans$allowWindchest(
-            BlockState state,
-            LevelReader level,
-            BlockPos pos,
-            CallbackInfoReturnable<Boolean> cir
-    ) {
-        BlockPos attachedPos = pos.relative(
-                WhistleBlock.getAttachedDirection(state)
-        );
-
-        BlockState attachedState = level.getBlockState(attachedPos);
-
-        if (attachedState.getBlock() instanceof WindchestBlock) {
-            cir.setReturnValue(true);
-        }
+    private boolean isTankOrWindchest(BlockState state, Operation<Boolean> original) {
+        return original.call(state) || state.getBlock() instanceof WindchestBlock;
     }
 }
